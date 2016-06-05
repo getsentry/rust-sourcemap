eb5ecfadd5db36bb5ae2ad1e31c7929adfc7de0f
Armin Ronacher <armin.ronacher@active-4.com>
2016-06-05 22:00:14 +0200

    Added nightly support

--- build.rs
+++ build.rs
@@ -1,18 +1,30 @@
-extern crate syntex;
-extern crate serde_codegen;
+#[cfg(not(feature="serde_macros"))]
+mod inner {
+    extern crate syntex;
+    extern crate serde_codegen;
 
-use std::env;
-use std::path::Path;
+    use std::env;
+    use std::path::Path;
 
 
-pub fn main() {
-    let out_dir = env::var_os("OUT_DIR").unwrap();
+    pub fn main() {
+        let out_dir = env::var_os("OUT_DIR").unwrap();
 
-    let src = Path::new("src/jsontypes.rs.in");
-    let dst = Path::new(&out_dir).join("jsontypes.rs");
+        let src = Path::new("src/jsontypes.rs.in");
+        let dst = Path::new(&out_dir).join("jsontypes.rs");
 
-    let mut registry = syntex::Registry::new();
+        let mut registry = syntex::Registry::new();
 
-    serde_codegen::register(&mut registry);
-    registry.expand("", &src, &dst).unwrap();
+        serde_codegen::register(&mut registry);
+        registry.expand("", &src, &dst).unwrap();
+    }
+}
+
+#[cfg(feature="serde_macros")]
+mod inner {
+    pub fn main() {}
+}
+
+fn main() {
+    inner::main();
 }
--- src/jsontypes.rs
+++ src/jsontypes.rs
@@ -1,1 +1,8 @@
+#![cfg_attr(feature="serde_macros", feature(custom_derive, plugin))]
+#![cfg_attr(feature="serde_macros", plugin(serde_macros))]
+
+#[cfg(feature="serde_macros")]
+include!("jsontypes.rs.in");
+
+#[cfg(not(feature="serde_macros"))]
 include!(concat!(env!("OUT_DIR"), "/jsontypes.rs"));
