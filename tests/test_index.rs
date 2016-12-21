bad26b01ec1418ce5a8568a150423e742e39f02c
Armin Ronacher <armin.ronacher@active-4.com>
2016-12-21 15:36:17 +0100

    Added a test for source contents setting

--- tests/test_index.rs
+++ tests/test_index.rs
@@ -38,20 +38,37 @@ fn test_basic_indexed_sourcemap() {
         ]
     }"#;
 
-    let f1 : Vec<_> = "function add(a, b) {\n \"use strict\";\n \
-                       return a + b; // fôo\n}".lines().collect();
-    let f2 : Vec<_> = "function multiply(a, b) {\n \"use strict\";\n \
-                       return a * b;\n}\nfunction divide(a, b) {\n \
-                       \"use strict\";\n try {\n  return multiply(add(a, \
-                       b), a, b) / c;\n } catch (e) {\n  \
-                       Raven.captureException(e);\n }\n}".lines().collect();
+    let f1 = "function add(a, b) {\n \"use strict\";\n \
+              return a + b; // fôo\n}";
+    let f2 = "function multiply(a, b) {\n \"use strict\";\n \
+              return a * b;\n}\nfunction divide(a, b) {\n \
+              \"use strict\";\n try {\n  return multiply(add(a, \
+              b), a, b) / c;\n } catch (e) {\n  \
+              Raven.captureException(e);\n }\n}";
 
-    let mut files = HashMap::new();
-    files.insert("file1.js", f1);
-    files.insert("file2.js", f2);
+    let mut raw_files = HashMap::new();
+    raw_files.insert("file1.js", f1);
+    raw_files.insert("file2.js", f2);
 
-    let ism = SourceMapIndex::from_reader(input).unwrap();
+    let mut files : HashMap<_, Vec<_>> = HashMap::new();
+    files.insert("file1.js", f1.lines().collect());
+    files.insert("file2.js", f2.lines().collect());
+
+    let mut ism = SourceMapIndex::from_reader(input).unwrap();
+    for section_id in 0..ism.get_section_count() {
+        let mut section = ism.get_section_mut(section_id).unwrap();
+        let mut map = section.get_sourcemap_mut().unwrap();
+        let contents = {
+            let filename = map.get_source(0).unwrap();
+            *raw_files.get(filename).unwrap()
+        };
+        map.set_source_contents(0, Some(contents));
+    }
     let flat_map = ism.flatten().unwrap();
+
+    let mut out : Vec<u8> = vec![];
+    flat_map.to_writer(&mut out);
+    println!("{}", String::from_utf8(out).unwrap());
 
     for token in flat_map.tokens() {
         let src = files.get(token.get_source().unwrap()).unwrap();
@@ -61,5 +78,12 @@ fn test_basic_indexed_sourcemap() {
             let span = &line[idx..idx + name.len()];
             assert_eq!(span, name);
         }
+    }
+
+    for (src_id, filename) in flat_map.sources().enumerate() {
+        let ref_contents = files.get(filename).unwrap();
+        let contents : Vec<_> = flat_map.get_source_contents(src_id as u32).expect(
+            &format!("no source for {}", filename)).lines().collect();
+        assert_eq!(&contents, ref_contents);
     }
 }
