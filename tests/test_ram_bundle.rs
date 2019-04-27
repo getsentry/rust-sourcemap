71bb60c29208a891b0820c277e21303a7ff2a3e6
Anton Ovchinnikov <anton@tonyo.info>
2019-04-27 19:30:56 +0200

    test: Change fixtures

--- tests/test_ram_bundle.rs
+++ tests/test_ram_bundle.rs
@@ -7,14 +7,15 @@ use std::path::Path;
 #[test]
 fn test_basic_ram_bundle() -> Result<(), std::io::Error> {
     let mut bundle_file = File::open("./tests/fixtures/ram_bundle/basic.jsbundle").unwrap();
+
     let mut bundle_data = Vec::new();
     bundle_file.read_to_end(&mut bundle_data)?;
     let ram_bundle = RamBundle::parse(&bundle_data).unwrap();
 
     // Header checks
-    assert_eq!(ram_bundle.module_count(), 367);
-    assert_eq!(ram_bundle.startup_code_size(), 0x32d2);
-    assert_eq!(ram_bundle.startup_code_offset(), 0xb84);
+    assert_eq!(ram_bundle.module_count(), 5);
+    assert_eq!(ram_bundle.startup_code_size(), 0x7192);
+    assert_eq!(ram_bundle.startup_code_offset(), 0x34);
 
     // Check first modules
     let mut module_iter = ram_bundle.iter_modules();
@@ -22,23 +23,23 @@ fn test_basic_ram_bundle() -> Result<(), std::io::Error> {
     let module_0 = module_iter.next().unwrap().unwrap();
     let module_0_data = module_0.data();
     assert_eq!(module_0.id(), 0);
-    assert_eq!(module_0_data.len(), 0x15b - 1);
+    assert_eq!(module_0_data.len(), 0xa8 - 1);
     assert_eq!(
-        module_0_data.get(0..40).unwrap(),
-        "__d(function(g,r,i,a,m,e,d){'use strict'".as_bytes()
+        module_0_data.get(0..60).unwrap(),
+        "__d(function(g,r,i,a,m,e,d){\"use strict\";const o=r(d[0]),s=r".as_bytes()
     );
 
-    let module_1 = module_iter.next().unwrap().unwrap();
-    let module_1_data = module_1.data();
-    assert_eq!(module_1.id(), 1);
-    assert_eq!(module_1_data.len(), 0xa5 - 1);
+    let module_3 = module_iter.next().unwrap().unwrap();
+    let module_3_data = module_3.data();
+    assert_eq!(module_3.id(), 3);
+    assert_eq!(module_3_data.len(), 0x6b - 1);
     assert_eq!(
-        module_1_data.get(0..40).unwrap(),
-        "__d(function(g,r,i,a,m,e,d){var n=r(d[0]".as_bytes()
+        module_3_data.get(0..60).unwrap(),
+        "__d(function(g,r,i,a,m,e,d){\"use strict\";console.log('inside".as_bytes()
     );
 
-    let module_2 = ram_bundle.get_module(2).unwrap();
-    assert!(module_2.is_none());
+    let module_1 = ram_bundle.get_module(1).unwrap();
+    assert!(module_1.is_none());
 
     Ok(())
 }
