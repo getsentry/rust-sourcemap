0987301a1d24039908110465e60eb5594604bc64
Anton Ovchinnikov <anton@tonyo.info>
2019-05-03 12:00:53 +0200

    ref: Flatten ram_bundle tests

--- src/ram_bundle.rs
+++ src/ram_bundle.rs
@@ -266,85 +266,80 @@ pub fn split_ram_bundle<'a, 'b>(
 }
 
 #[cfg(test)]
-mod tests {
-    use super::*;
-    use std::fs::File;
-    use std::io::Read;
+use {std::fs::File, std::io::Read};
 
-    #[test]
-    fn test_basic_ram_bundle_parse() -> std::result::Result<(), Box<std::error::Error>> {
-        let mut bundle_file = File::open("./tests/fixtures/ram_bundle/basic.jsbundle")?;
-        let mut bundle_data = Vec::new();
-        bundle_file.read_to_end(&mut bundle_data)?;
-        let ram_bundle = RamBundle::parse(&bundle_data)?;
+#[test]
+fn test_basic_ram_bundle_parse() -> std::result::Result<(), Box<std::error::Error>> {
+    let mut bundle_file = File::open("./tests/fixtures/ram_bundle/basic.jsbundle")?;
+    let mut bundle_data = Vec::new();
+    bundle_file.read_to_end(&mut bundle_data)?;
+    let ram_bundle = RamBundle::parse(&bundle_data)?;
 
-        // Header checks
-        assert_eq!(ram_bundle.module_count(), 5);
-        assert_eq!(ram_bundle.startup_code_size, 0x7192);
-        assert_eq!(ram_bundle.startup_code_offset, 0x34);
+    // Header checks
+    assert_eq!(ram_bundle.module_count(), 5);
+    assert_eq!(ram_bundle.startup_code_size, 0x7192);
+    assert_eq!(ram_bundle.startup_code_offset, 0x34);
 
-        // Check first modules
-        let mut module_iter = ram_bundle.iter_modules();
+    // Check first modules
+    let mut module_iter = ram_bundle.iter_modules();
 
-        let module_0 = module_iter.next().unwrap()?;
-        let module_0_data = module_0.data();
-        assert_eq!(module_0.id(), 0);
-        assert_eq!(module_0_data.len(), 0xa8 - 1);
-        assert_eq!(
-            &module_0_data[0..60],
-            "__d(function(g,r,i,a,m,e,d){\"use strict\";const o=r(d[0]),s=r".as_bytes()
-        );
+    let module_0 = module_iter.next().unwrap()?;
+    let module_0_data = module_0.data();
+    assert_eq!(module_0.id(), 0);
+    assert_eq!(module_0_data.len(), 0xa8 - 1);
+    assert_eq!(
+        &module_0_data[0..60],
+        "__d(function(g,r,i,a,m,e,d){\"use strict\";const o=r(d[0]),s=r".as_bytes()
+    );
 
-        let module_3 = module_iter.next().unwrap()?;
-        let module_3_data = module_3.data();
-        assert_eq!(module_3.id(), 3);
-        assert_eq!(module_3_data.len(), 0x6b - 1);
-        assert_eq!(
-            &module_3_data[0..60],
-            "__d(function(g,r,i,a,m,e,d){\"use strict\";console.log('inside".as_bytes()
-        );
+    let module_3 = module_iter.next().unwrap()?;
+    let module_3_data = module_3.data();
+    assert_eq!(module_3.id(), 3);
+    assert_eq!(module_3_data.len(), 0x6b - 1);
+    assert_eq!(
+        &module_3_data[0..60],
+        "__d(function(g,r,i,a,m,e,d){\"use strict\";console.log('inside".as_bytes()
+    );
 
-        let module_1 = ram_bundle.get_module(1)?;
-        assert!(module_1.is_none());
+    let module_1 = ram_bundle.get_module(1)?;
+    assert!(module_1.is_none());
 
-        Ok(())
-    }
+    Ok(())
+}
 
-    #[test]
-    fn test_basic_ram_bundle_split() -> std::result::Result<(), Box<std::error::Error>> {
-        let mut bundle_file = File::open("./tests/fixtures/ram_bundle/basic.jsbundle")?;
-        let mut bundle_data = Vec::new();
-        bundle_file.read_to_end(&mut bundle_data)?;
-        let ram_bundle = RamBundle::parse(&bundle_data)?;
+#[test]
+fn test_basic_ram_bundle_split() -> std::result::Result<(), Box<std::error::Error>> {
+    let mut bundle_file = File::open("./tests/fixtures/ram_bundle/basic.jsbundle")?;
+    let mut bundle_data = Vec::new();
+    bundle_file.read_to_end(&mut bundle_data)?;
+    let ram_bundle = RamBundle::parse(&bundle_data)?;
 
-        let sourcemap_file = File::open("./tests/fixtures/ram_bundle/basic.jsbundle.map")?;
-        let ism = SourceMapIndex::from_reader(sourcemap_file)?;
+    let sourcemap_file = File::open("./tests/fixtures/ram_bundle/basic.jsbundle.map")?;
+    let ism = SourceMapIndex::from_reader(sourcemap_file)?;
 
-        assert!(ism.is_for_react_native());
+    assert!(ism.is_for_react_native());
 
-        let x_facebook_offsets = ism.x_facebook_offsets().unwrap();
-        assert_eq!(x_facebook_offsets.len(), 5);
+    let x_facebook_offsets = ism.x_facebook_offsets().unwrap();
+    assert_eq!(x_facebook_offsets.len(), 5);
 
-        let x_metro_module_paths = ism.x_metro_module_paths().unwrap();
-        assert_eq!(x_metro_module_paths.len(), 7);
+    let x_metro_module_paths = ism.x_metro_module_paths().unwrap();
+    assert_eq!(x_metro_module_paths.len(), 7);
 
-        // Modules 0, 3, 4
-        assert_eq!(split_ram_bundle(&ram_bundle, &ism)?.count(), 3);
+    // Modules 0, 3, 4
+    assert_eq!(split_ram_bundle(&ram_bundle, &ism)?.count(), 3);
 
-        let mut ram_bundle_iter = split_ram_bundle(&ram_bundle, &ism)?;
+    let mut ram_bundle_iter = split_ram_bundle(&ram_bundle, &ism)?;
 
-        let (name, sourceview, sourcemap) = ram_bundle_iter.next().unwrap()?;
-        assert_eq!(name, "0.js");
-        assert_eq!(
-            &sourceview.source()[0..60],
-            "__d(function(g,r,i,a,m,e,d){\"use strict\";const o=r(d[0]),s=r"
-        );
-        assert_eq!(
-            &sourcemap.get_source_contents(0).unwrap()[0..60],
-            "const f = require(\"./other\");\nconst isWindows = require(\"is-"
-        );
+    let (name, sourceview, sourcemap) = ram_bundle_iter.next().unwrap()?;
+    assert_eq!(name, "0.js");
+    assert_eq!(
+        &sourceview.source()[0..60],
+        "__d(function(g,r,i,a,m,e,d){\"use strict\";const o=r(d[0]),s=r"
+    );
+    assert_eq!(
+        &sourcemap.get_source_contents(0).unwrap()[0..60],
+        "const f = require(\"./other\");\nconst isWindows = require(\"is-"
+    );
 
-        Ok(())
-    }
-
+    Ok(())
 }
