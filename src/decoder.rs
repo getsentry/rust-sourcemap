5b4c7f3f089bfd6054e8f058703ffe9fa2e1a7a0
Floris Bruynooghe <flub@sentry.io>
2022-01-13 10:52:32 +0100

    ref(clippy): Use unwrap_or_default (#39)

--- src/decoder.rs
+++ src/decoder.rs
@@ -128,7 +128,7 @@ pub fn decode_regular(rsm: RawSourceMap) -> Result<SourceMap> {
     let mut name_id = 0;
 
     let names = rsm.names.unwrap_or_default();
-    let sources = rsm.sources.unwrap_or_else(Vec::new);
+    let sources = rsm.sources.unwrap_or_default();
     let mappings = rsm.mappings.unwrap_or_else(|| "".into());
     let allocation_size = mappings.matches(&[',', ';'][..]).count() + 10;
     let mut tokens = Vec::with_capacity(allocation_size);
