cbcebd34c55caa6834693780f7df3f39b2064c1f
Kamil Ogórek <kamil.ogorek@gmail.com>
2022-09-28 17:48:11 +0200

    feat: Add source_root handling support (#51)
    * feat: Add source_root handling support

---
+++ tests/test_builder.rs
@@ -0,0 +1,19 @@
+use sourcemap::SourceMapBuilder;
+
+#[test]
+fn test_builder_into_sourcemap() {
+    let mut builder = SourceMapBuilder::new(None);
+    builder.set_source_root(Some("/foo/bar"));
+    builder.add_source("baz.js");
+    builder.add_name("x");
+
+    let sm = builder.into_sourcemap();
+    assert_eq!(sm.get_source_root(), Some("/foo/bar"));
+    assert_eq!(sm.get_source(0), Some("baz.js"));
+    assert_eq!(sm.get_name(0), Some("x"));
+
+    let expected = b"{\"version\":3,\"sources\":[\"baz.js\"],\"sourceRoot\":\"/foo/bar\",\"names\":[\"x\"],\"mappings\":\"\"}";
+    let mut output: Vec<u8> = vec![];
+    sm.to_writer(&mut output).unwrap();
+    assert_eq!(output, expected);
+}
