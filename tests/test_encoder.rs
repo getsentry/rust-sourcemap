46d337dfbcdb752670454eccf8158518693cb98c
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-01-25 15:55:18 +0900

    Add a test

--- tests/test_encoder.rs
+++ tests/test_encoder.rs
@@ -25,3 +25,23 @@ fn test_sourcemap_data_url() {
     let sm = SourceMap::from_reader(input).unwrap();
     assert_eq!(sm.to_data_url().unwrap(), "data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYnVpbGQvZm9vLm1pbi5qcyIsInNvdXJjZXMiOlsic3JjL2Zvby5qcyJdLCJzb3VyY2VSb290IjoiLyIsIm5hbWVzIjpbXSwibWFwcGluZ3MiOiJBQUFBIn0=");
 }
+
+#[test]
+fn test_basic_range() {
+    let input: &[_] = br#"{
+        "version": 3,
+        "sources": [null],
+        "names": ["console","log","ab"],
+        "mappings": "AACAA,QAAQC,GAAG,CAAC,OAAM,OAAM,QACxBD,QAAQC,GAAG,CAAC,QAEZD,QAAQC,GAAG,CAJD;IAACC,IAAI;AAAI,IAKnBF,QAAQC,GAAG,CAAC,YACZD,QAAQC,GAAG,CAAC",
+        "rangeMappings": "AAB;;g"
+    }"#;
+    let sm = SourceMap::from_reader(input).unwrap();
+    let mut out: Vec<u8> = vec![];
+    sm.to_writer(&mut out).unwrap();
+
+    let sm2 = SourceMap::from_reader(&out[..]).unwrap();
+
+    for (tok1, tok2) in sm.tokens().zip(sm2.tokens()) {
+        assert_eq!(tok1, tok2);
+    }
+}
