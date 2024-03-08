2a9cec387b40d55b4b2049d6b47d28c2f30c97be
underfin <likui.underfin@gmail.com>
2024-03-08 15:52:16 +0800

    feat: add SourceMap::to_data_url

--- tests/test_encoder.rs
+++ tests/test_encoder.rs
@@ -18,3 +18,10 @@ fn test_basic_sourcemap() {
         assert_eq!(tok1, tok2);
     }
 }
+
+#[test]
+fn test_sourcemap_data_url() {
+    let input: &[_] = br#"{"version":3,"file":"build/foo.min.js","sources":["src/foo.js"],"names":[],"mappings":"AAAA","sourceRoot":"/"}"#;
+    let sm = SourceMap::from_reader(input).unwrap();
+    assert_eq!(sm.to_data_url().unwrap(), "data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYnVpbGQvZm9vLm1pbi5qcyIsInNvdXJjZXMiOlsic3JjL2Zvby5qcyJdLCJzb3VyY2VSb290IjoiLyIsIm5hbWVzIjpbXSwibWFwcGluZ3MiOiJBQUFBIn0=");
+}
