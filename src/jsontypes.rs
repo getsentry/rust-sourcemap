1193b4bb74e2085128f7c12650db47fb5717f7e7
Sebastian Zivota <loewenheim@mailbox.org>
2024-11-26 10:34:53 +0100

    Fix serialization

--- src/jsontypes.rs
+++ src/jsontypes.rs
@@ -58,7 +58,7 @@ pub struct RawSourceMap {
     pub debug_id: Option<DebugId>,
     // This field only exists to be able to deserialize from "debug_id" keys
     // if "debugId" is unset.
-    #[serde(skip_serializing, rename = "debug_id")]
+    #[serde(skip_serializing_if = "Option::is_none", rename = "debug_id")]
     pub debug_id_old: Option<DebugId>,
 }
 
